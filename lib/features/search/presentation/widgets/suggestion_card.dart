55a21742548adf367a96f76044477eac1c4927df
Ayat Esaam <eayatessam1@gmail.com>
2026-06-05 16:16:39 +0300

    feat(home): add Notification screen with dummy data

--- lib/features/search/presentation/widgets/suggestion_card.dart
+++ lib/features/search/presentation/widgets/suggestion_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/theme/app_radius.dart';
 import '../../../../core/theme/app_spacing.dart';
