bcbb569cce2d871d013efea1011e25a197fb7d2c
rajeh1032 <ahmmedra209@gmail.com>
2026-07-02 04:27:07 +0300

    feat: Update iOS project configuration and dependencies
    - Added Podfile.lock to manage CocoaPods dependencies.
    - Updated Runner.xcodeproj to include new frameworks and configurations.
    - Created Package.resolved for Swift Package Manager dependencies.
    - Modified Runner.xcscheme to include a pre-action for preparing Flutter framework.
    - Updated Runner.xcworkspace to reference Pods project.
    - Disabled DevicePreview in main.dart for release mode.
    - Updated pubspec.lock to reflect new versions of dependencies (meta: 1.18.0, test_api: 0.7.11).
    - Fixed API endpoint in api_endpoints_test.dart to include versioning in the URL.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
@@ -33,7 +32,7 @@ Future<void> main() async {
 
   runApp(
     DevicePreview(
-      enabled: !kReleaseMode,
+      enabled: false,
       availableLocales: LocalizationService.supportedLocales,
       builder: (_) => EasyLocalization(
         supportedLocales: LocalizationService.supportedLocales,
